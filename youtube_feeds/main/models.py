2e596c35d17c34a4483f9c5e7ed4b4b0cdc4a36f
Phuong Nguyen <phuong.nguyen@inspilab.com>
2019-06-22 15:15:14 +0700

    Update model YoutubeVideo: remove html field and add description field

--- youtube_feeds/main/models.py
+++ youtube_feeds/main/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class YoutubeVideo(models.Model):
     vid = models.CharField(primary_key=True, max_length=100, db_index=True)
     title = models.CharField(max_length=255)
-    html = models.TextField()
+    description = models.TextField(default="Video description here.")
     shared_by = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='shared_videos')
     shared_at = models.DateTimeField(auto_now_add=True, db_index=True)
