f2390117ef8dbcfe31872a859ab405c75ebc0757
Phuong Nguyen <phuong.nguyen@inspilab.com>
2019-06-22 14:37:45 +0700

    Add new model to save shared videos

--- youtube_feeds/main/models.py
+++ youtube_feeds/main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class YoutubeVideo(models.Model):
+    vid = models.CharField(primary_key=True, max_length=100, db_index=True)
+    title = models.CharField(max_length=255)
+    html = models.TextField()
+    shared_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='shared_videos')
+    shared_at = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    class Meta:
+        db_table = "youtube_video"
