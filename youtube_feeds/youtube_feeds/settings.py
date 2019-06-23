b527925f48ff1f7e392c6baa47e1f5b32089b4ad
Phuong Nguyen <phuong.nguyen@inspilab.com>
2019-06-23 15:18:00 +0700

    Update required packages and setting whitenoise to serve static file on production

--- youtube_feeds/youtube_feeds/settings.py
+++ youtube_feeds/youtube_feeds/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,3 +121,5 @@ USE_TZ = True
 STATIC_ROOT = '/static/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+#  Add configuration for static files storage using whitenoise
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
